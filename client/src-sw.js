34b29ed43b3950cc209bd8b57f5188663d84ae71
pocketclint <118079528+pocketclint@users.noreply.github.com>
2023-05-06 16:31:06 -0500

    stale while revalidate?

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,5 +1,5 @@
 const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst } = require('workbox-strategies');
+const { CacheFirst, StaleWhileRevalidate } = require('workbox-strategies');
 const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
@@ -31,7 +31,7 @@ registerRoute(
   ({ request }) => request.destination === 'style' ||
     request.destination === 'script' ||
     request.destination === 'worker',
-  new CacheFirst({
+  new StaleWhileRevalidate({
     cacheName: 'asset-cache',
     plugins: [
       new CacheableResponsePlugin({
